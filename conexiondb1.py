c537d6a3de7d617745f4ea6ac149f5d2e859a88c
luzmsantos <luzsantos2006@gmail.com>
2015-12-10 16:21:24 -0800

    vista de datos

--- conexiondb1.py
+++ conexiondb1.py
@@ -8,19 +8,12 @@ db = MySQLdb.connect('127.0.0.1', 'root', '12345', 'body-db0')
 
 cursor = db.cursor()
 
-# ejecutar una consulta
-
-result = cursor.execute("SELECT lectura FROM tbody2")
-
-result_string = json.load(result)
-print 'ENCODED:', result_string
-
-decoded = json.load(result_string)
-print 'DECOCED:', decoded
-
-#cursor.fetchall()
+result = cursor.execute("SELECT claveU FROM tbody2")
 
 
+tbody = [row[0] for row in cursor.fetchall()]
+
+print 'total de datos: %s' % (tbody)
 
 cursor.close()
 
