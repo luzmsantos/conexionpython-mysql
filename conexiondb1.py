199df6d575dccf979f8aff8340d928f5b82c9c2c
luzmsantos <luzsantos2006@gmail.com>
2015-12-17 12:16:08 -0800

    consulta

--- conexiondb1.py
+++ conexiondb1.py
@@ -1,20 +1,56 @@
-__author__ = 'MCC1'
+
 
 import MySQLdb
-import json
+#import json
 
 
 db = MySQLdb.connect('127.0.0.1', 'root', '12345', 'body-db0')
 
 cursor = db.cursor()
 
-result = cursor.execute("SELECT claveU FROM tbody2")
+result = cursor.execute("SELECT * FROM tbody2 WHERE claveU='4F936A16677548F7B408226F54323881' ")
+
+#tbody = [row[0] for row in cursor.fetchall()]
+
+#lista= json.load('cursor.lectura')
+
+#lista['head', 'neck', 'hipLeft']
+row = cursor.fetchone()
+
+for row in cursor.fetchall():
+    print row
+    row = cursor.fetchone()
+
+#for i in row:
+#      print i
+row = cursor.fetchone()
+
+#db = MySQLdb.connect('127.0.0.1', 'root', '12345', 'body-db0')
+
+#cursor = db.cursor()
+
+#result = cursor.execute("SELECT * FROM tbody2")
+
+#row = cursor.fetchone()
+
+#while row is not None:
+#    print (row)
+#    row = cursor.fetchone()
 
 
-tbody = [row[0] for row in cursor.fetchall()]
 
-print 'total de datos: %s' % (tbody)
+
+
+#tbodyr =
+
+
+#print 'total de datos:|| %s \n' % (tbody)
+
+
+
 
 cursor.close()
-
 db.close()
+
+
+#tbody = [row[0] for row in cursor.fetchall()]
